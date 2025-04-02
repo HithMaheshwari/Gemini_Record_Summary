c8532501839d435444b70d22d0023e6b7dbc3699
HithMaheshwari <sahilmaheshwari012@gmail.com>
2025-04-02 14:24:41 +0530

    Added Summary Button

---
+++ force-app/main/default/lwc/recordSummaryButton/recordSummaryButton.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class RecordSummaryButton extends LightningElement {
+    handleClick() {
+        // Your record summary logic here
+        alert('Record Summary button clicked!');
+    }
+}
